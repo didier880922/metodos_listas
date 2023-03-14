9aeefddc402d1e55dd270e1c77835b4aa06fd37e
DIDIER FERNANDO GUERRERO SUMALAVE <67668036+didier880922@users.noreply.github.com>
2023-03-14 11:55:05 -0500

    CAMBIO
    CAMBIO

---
+++ metodosListas_14032023.py
@@ -0,0 +1,30 @@
+data = []
+
+def menu():
+    print("APRENDIENDO LOS METODOS DE LISTAS")
+    print("1. Metodo Append")
+    print("2. Metodo Index")
+    print("3. Metodo Count")
+    print("4. Metodo Clear")
+    print("5. Metodo Extend")
+    print("6. Metodo Remove")
+    print("7. Metodo Len")
+    print("8. Metodo Pop")
+    print("9. Metodo Sort")
+    print("10. Metodo Insert")
+    print("11. Metodo Reverse")
+    opcion = int(input("Digita una opción: "))
+
+    if (opcion == 1):
+        pass
+    elif (opcion == 2):
+        pass
+    elif (opcion == 3): 
+        pass
+
+#Resto de funciones
+
+
+#Codigo principal
+if __name__ == "__main__":
+    menu()
